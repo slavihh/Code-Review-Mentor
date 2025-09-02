7a3e8a23a1c066a38b76c823c95873a9dd7d3f88
Slavko Hinkin <slavihh@gmail.com>
2025-09-02 19:55:20 +0300

    ruff check

--- app/schemas/submissions.py
+++ app/schemas/submissions.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict
-from typing import Any, Optional
+from typing import Optional
 from datetime import datetime
 from uuid import UUID
 from app.models.postgre import Language
